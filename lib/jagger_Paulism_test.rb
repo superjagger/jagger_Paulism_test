b2937c31da386e51154d1d6ed74b3ac2eae38c6d
superjagger <31763819+superjagger@users.noreply.github.com>
2024-03-02 22:23:17 +0800

    Add files via upload

---
+++ lib/jagger_Paulism_test.rb
@@ -0,0 +1,6 @@
+require "jagger_Paulism_test/version"
+
+module JaggerPaulismTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jagger_Paulism_test/version.rb
@@ -0,0 +1,3 @@
+module JaggerPaulismTest
+  VERSION = "0.1.0"
+end
